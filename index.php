a0514a4a244993f0a5cec2fca1358206dfce1c6a
max <maxence.landa@gmail.com>
2024-03-06 09:53:32 +0100

    structure

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./style.css" rel="stylesheet"/>
+    <title>Document</title>
+</head>
+<body>
+    <div class="card">
+        <img src=""/>
+        <p>Name</p>
+        <a href="./form.php">Upload</a>
+    </div>
+</body>
+</html>
