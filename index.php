ccc1e7a9d8ee387810173d8fb94fc40024243847
max <maxence.landa@gmail.com>
2024-03-06 10:55:25 +0100

    style index

--- index.php
+++ index.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="./style.css" rel="stylesheet"/>
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Document</title>
 </head>
-<body>
-    <div class="card">
-        <img src=""/>
-        <p>Name</p>
-        <a href="./form.php">Upload</a>
+<body class="bg-gradient-to-b from-fuchsia-300 to-violet-400">
+    <div class="w-1/4 mx-auto mt-32 bg-gradient-to-b from-white text-center rounded-lg">
+        <p class="font-semibold text-3xl pt-5">EPSILON</p>
+        <p>Plateforme de peer-learning</p>
+        <p>EPSI Lille</p>
+        <div class="max-w-44 max-h-88 mx-auto rounded-full overflow-hidden my-5">
+            <img src="./assets/maxenss.webp"/>
+        </div>
+        <p>Maxence LANDA</p>
+        <a href="./form.php" class="underline">Upload your file !</a>
     </div>
 </body>
 </html>
