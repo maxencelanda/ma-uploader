9d499580aec5dacde2ef824eba73b5f9fe3e897c
max <maxence.landa@gmail.com>
2024-03-07 10:02:12 +0100

    header + confirmation d'upload

--- form.php
+++ form.php
@@ -1,11 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./style.css" rel="stylesheet"/>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Document</title>
+    <?php include "./header.php" ?>
+    <title>EPSILON - Upload</title>
 </head>
 <body class="bg-no-repeat min-h-[100vh] bg-gradient-to-b from-fuchsia-300 to-violet-400">
     <div class="w-1/4 mx-auto mt-32 bg-gradient-to-b from-white text-center rounded-lg">
@@ -23,7 +20,9 @@ if (isset($_POST["submit"])){
     $targetDirectory = "./uploadedFiles/";
     $fileToUpload = $_FILES["uploadedFile"];
     $fileDirectory = $targetDirectory . basename($fileToUpload["name"]);
-    move_uploaded_file($fileToUpload["tmp_name"], $fileDirectory);
+    if (move_uploaded_file($fileToUpload["tmp_name"], $fileDirectory)){
+        echo "<p class='text-center'>Document uploadé avec succès.</p>";
+    }
 }
 
 ?>
